b99f1f59c023e32a86428b32542bac0842833d54
Ferdinand Schwertner <ferdinandschwertner@yahoo.de>
2024-04-18 09:45:44 +0200

    adding frontend

---
+++ target/classes/static/js/crud_demo_simplemethods.js
@@ -0,0 +1,12 @@
+function clearUpdate(){
+    $('#updateInput1').val('');
+    $('#updateInput2').val('');
+    $('#updateInput3').val('');
+    $('#updateInput4').val('');
+}
+
+function clearCreate(){
+    $('#createInput1').val('');
+    $('#createInput2').val('');
+    $('#createInput3').val('');
+}
